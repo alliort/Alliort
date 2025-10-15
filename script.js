6c67962b96256527e764208cd743e979bdfcfbd5
alliort <iforgotgt@gmail.com>
2025-10-15 12:59:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,13 @@ const shorts = [
   // ...add all 39 shorts here
 ];
 
+const toggleBtn = document.getElementById('toggleSidebar');
+const sidebar = document.getElementById('sidebar');
+
+toggleBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('open');
+});
+
 const container = document.getElementById('shorts-list');
 shorts.forEach(id => {
   const link = document.createElement('a');
