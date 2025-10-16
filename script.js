3d7c7ea1cb7b31f687b62c5695226c2344096f10
alliort <iforgotgt@gmail.com>
2025-10-15 22:10:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Site loaded!");
+<script>
+  const button = document.getElementById('toggle-shorts');
+  const shortsTitle = document.querySelector('.shorts-title');
+  const shortsContainer = document.querySelector('.shorts-container');
+
+  button.addEventListener('click', () => {
+    // toggle visibility
+    if (shortsContainer.style.display === 'none') {
+      shortsContainer.style.display = 'flex'; // show section
+      shortsTitle.style.display = 'block';    // show title
+    } else {
+      shortsContainer.style.display = 'none'; // hide section
+      shortsTitle.style.display = 'none';     // hide title
+    }
+  });
+</script>
