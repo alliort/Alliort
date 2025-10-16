6ae6a2327146c45f46f633f978e973ba96e44794
alliort <iforgotgt@gmail.com>
2025-10-15 22:25:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ const shortsTitle = document.querySelector('.shorts-title');
 const shortsContainer = document.querySelector('.shorts-container');
 
 button.addEventListener('click', () => {
-  const isHidden = shortsContainer.style.display === 'none' || shortsContainer.style.display === '';
+  const isVisible = window.getComputedStyle(shortsContainer).display !== 'none';
   
-  if (isHidden) {
-    shortsContainer.style.display = 'flex';
-    shortsTitle.style.display = 'block';
-  } else {
+  if (isVisible) {
     shortsContainer.style.display = 'none';
     shortsTitle.style.display = 'none';
+  } else {
+    shortsContainer.style.display = 'flex';
+    shortsTitle.style.display = 'block';
   }
 });
 </script>
