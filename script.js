86656c5685d3a254c8808228e23227d5b8e19889
alliort <iforgotgt@gmail.com>
2025-10-15 22:34:57 -0500

    Clear script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-<script>
-document.addEventListener('DOMContentLoaded', () => {
-  const button = document.getElementById('toggle-shorts');
-  const shortsTitle = document.querySelector('.shorts-title');
-  const shortsContainer = document.querySelector('.shorts-container');
 
-  button.addEventListener('click', () => {
-    const isVisible = window.getComputedStyle(shortsContainer).display !== 'none';
-    shortsContainer.style.display = isVisible ? 'none' : 'flex';
-    shortsTitle.style.display = isVisible ? 'none' : 'block';
-  });
-});
-</script>
