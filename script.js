52746820122b8c699a124c497e85c31a9c3f4c12
alliort <iforgotgt@gmail.com>
2025-10-15 22:19:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 <script>
-  const button = document.getElementById('toggle-shorts');
-  const shortsTitle = document.querySelector('.shorts-title');
-  const shortsContainer = document.querySelector('.shorts-container');
+const button = document.getElementById('toggle-shorts');
+const shortsTitle = document.querySelector('.shorts-title');
+const shortsContainer = document.querySelector('.shorts-container');
 
-  button.addEventListener('click', () => {
-    if (shortsContainer.style.display === 'none') {
-      shortsContainer.style.display = 'flex';
-      shortsTitle.style.display = 'block';
-    } else {
-      shortsContainer.style.display = 'none';
-      shortsTitle.style.display = 'none';
-    }
-  });
+button.addEventListener('click', () => {
+  const isHidden = shortsContainer.style.display === 'none' || shortsContainer.style.display === '';
+  
+  if (isHidden) {
+    shortsContainer.style.display = 'flex';
+    shortsTitle.style.display = 'block';
+  } else {
+    shortsContainer.style.display = 'none';
+    shortsTitle.style.display = 'none';
+  }
+});
 </script>
